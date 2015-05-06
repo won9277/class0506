d541856596f957569cd7a135ce8948788d61c53c
jaeseung/icto55-2 <won9277@naver.com>
2015-05-06 17:31:13 +0900

    수업시간 커밋

---
+++ WebContent/insert.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+	//이메일을 등록합니다.
+	response.sendRedirect( "/emaillist/show_emaillist.jsp");
+%>
---
+++ src/com/sds/icto/emaillist/dao/EmailListDao.java
@@ -0,0 +1,17 @@
+package com.sds.icto.emaillist.dao;
+
+import java.util.*;
+
+import com.sds.icto.emaillist.vo.EmailListVo;
+
+public class EmailListDao {
+	
+	public void insert(){
+		
+	}
+	
+	public List<EmailListVo> fetchList(){
+		return null;
+	}
+
+}
